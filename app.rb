5d26bfaf4e0fa49f60c37e10b9deff61992ccbb4
Asia Kane and Susha Dore <asia@>
2017-05-08 13:33:59 -0700

    add routing and erb files to list and create a division on homepage

--- app.rb
+++ app.rb
@@ -2,10 +2,25 @@ require 'sinatra'
 require 'sinatra/reloader'
 require 'sinatra/activerecord'
 require './lib/employee'
+require './lib/division'
+require './lib/project'
 require 'pry'
 
 also_reload('lib/**/*.rb')
 
-get('/') do
-  erb(:index)
+get '/' do
+  @divisions = Division.all
+  erb :index
+end
+
+get '/divisions/:id' do
+  id = params['id'].to_i
+  @division = Division.find(id)
+  erb :division
+end
+
+post '/divisions' do
+  @divisions = Division.all
+  division = Division.create({:title => params['title']})
+  erb :index
 end
