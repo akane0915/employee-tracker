d669b6df9dda90ffd4db396d13a3347051ea73b7
Asia Kane and Susha Dore <susha@>
2017-05-08 11:57:37 -0700

    pass 2nd Employee spec, #project

--- spec/employee_spec.rb
+++ spec/employee_spec.rb
@@ -9,4 +9,12 @@ describe Employee do
       expect(employee.division).to eq division
     end
   end
+
+  describe '#project' do
+    it 'returns the project an employee belongs to' do
+      project = Project.create({:description => 'do things'})
+      employee = Employee.create({:name => 'Joe Bob', :project_id => project.id})
+      expect(employee.project).to eq project
+    end
+  end
 end
